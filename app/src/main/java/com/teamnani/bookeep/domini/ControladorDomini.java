fa99bf29fc42ba2baaaeadd700ee75602c1b0cd8
jacknick7 <josep.ciurana@est.fib.upc.edu>
2017-11-11 14:57:55 +0100

    Noves Classes Domini

--- app/src/main/java/com/teamnani/bookeep/domini/ControladorDomini.java
+++ app/src/main/java/com/teamnani/bookeep/domini/ControladorDomini.java
@@ -1,18 +1,31 @@
 package com.teamnani.bookeep.domini;
 
+import android.support.v7.app.AppCompatActivity;
+import android.util.AndroidException;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
 import com.teamnani.bookeep.dades.ControladorDades;
 import com.teamnani.bookeep.presentacio.ControladorPresentacio;
+
+import java.util.ArrayList;
 
 /**
  * Created by oriolclosa on 11/11/17.
  */
 
-public class ControladorDomini {
+public class ControladorDomini extends AppCompatActivity {
     private ControladorPresentacio ctrlPresentacio;
     private ControladorDades ctrlDades;
 
     public ControladorDomini(ControladorPresentacio pCtrlPresentacio){
         ctrlPresentacio = pCtrlPresentacio;
         ctrlDades = new ControladorDades();
+    }
+
+    public ArrayList<Llibre> obtenirNous() {
+        ArrayList<Llibre> t = new ArrayList<>();
+        t.add(new Llibre("Aquest es el titol", 1997, "La meva editorial", new ImageView(this), (float) 3.2,4));
+        return t;
     }
 }
