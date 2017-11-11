4793919a744bb89c40afe258a17e35d6209ffb4a
mt.oriol97@gmail.com <mt.oriol97@gmail.com>
2017-11-11 21:18:23 +0100

    Stranger things

---
+++ app/src/main/java/com/teamnani/bookeep/dades/NetworkUtils.java
@@ -0,0 +1,76 @@
+package com.teamnani.bookeep.dades;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Muniategui on 11/11/2017.
+ */
+
+public class NetworkUtils {
+    private static final String LOG_TAG = NetworkUtils.class.getSimpleName();
+    private static final String BOOK_BASE_URL = "https://www.googleapis.com/books/v1/volumes?";
+    private static final String QUERY_PARM = "q";
+    private static final String MAX_RESULTS = "maxResults";
+    private static final String PRINT_TYPE = "printType";
+
+    static String getBookInfo(String queryString){
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String bookJSONString = null;
+
+        try {
+            Uri buildUri = Uri.parse(BOOK_BASE_URL).buildUpon()
+                    .appendQueryParameter(QUERY_PARM,queryString)
+                    .appendQueryParameter(MAX_RESULTS, "40")
+                    .appendQueryParameter(PRINT_TYPE, "books")
+                    .build();
+
+            URL requestURL = new URL(buildUri.toString());
+
+            urlConnection = (HttpURLConnection) requestURL.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null){
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line + "\n");
+            }
+            if (buffer.length() == 0){
+                return null;
+            }
+            bookJSONString = buffer.toString();
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return null;
+        }finally{
+            if (urlConnection!=null){
+                urlConnection.disconnect();
+            }
+            if (reader != null){
+                try{
+                    reader.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            Log.d(LOG_TAG, bookJSONString);
+            return bookJSONString;
+        }
+    }
+
+}
