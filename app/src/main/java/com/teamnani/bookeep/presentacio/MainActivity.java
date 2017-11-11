8e6738b07cabde7770fdfd1e443fa91cb19aca98
oriol20 <oriol20@gmail.com>
2017-11-11 13:58:55 +0100

    Arreglades les activitats

--- app/src/main/java/com/teamnani/bookeep/presentacio/MainActivity.java
+++ app/src/main/java/com/teamnani/bookeep/presentacio/MainActivity.java
@@ -2,6 +2,9 @@ package com.teamnani.bookeep.presentacio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.bookeep.teamnani.bookeep.R;
 
@@ -11,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout starred = (LinearLayout) findViewById(R.id.starredLayout);
+        LinearLayout starred2 = new LinearLayout(this);
+        starred2.setOrientation(LinearLayout.HORIZONTAL);
+        TextView prova = new TextView(this);
+        prova.setText("Prova");
+        starred2.addView(prova);
+        starred.addView(starred2);
     }
 }
