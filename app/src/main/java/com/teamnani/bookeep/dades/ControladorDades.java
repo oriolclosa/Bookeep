82a05b2f01049f2aa7dd83dbd5499beb98e97b3b
Muniategui <jdemuniategui@hotmail.com>
2017-11-12 05:23:44 +0100

    /

--- app/src/main/java/com/teamnani/bookeep/dades/ControladorDades.java
+++ app/src/main/java/com/teamnani/bookeep/dades/ControladorDades.java
@@ -92,7 +92,7 @@ public class ControladorDades {
         GoogleAPIBook api = new GoogleAPIBook();
         try {
 
-            api.execute("inauthor:"+s, "llibres", "relevance").get();
+            api.execute("inauthor:"+s, "books", "relevance").get();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
@@ -111,7 +111,7 @@ public class ControladorDades {
         GoogleAPIBook api = new GoogleAPIBook();
         try {
 
-            api.execute("intitle:"+s, "llibres", "relevance").get();
+            api.execute("intitle:"+s, "books", "relevance").get();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
